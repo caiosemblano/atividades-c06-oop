f144112cf13ae1f9b32d996a4038909848218a61
Caio Semblano Silva <semblanocaio@gmail.com>
2026-02-25 22:45:02 -0300

    exercicio 4 completo

---
+++ src/main/java/exercicio4/Main.java
@@ -0,0 +1,27 @@
+package exercicio4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Informe a nota da NPA: ");
+        final short NPA =  input.nextShort();
+
+        if(NPA < 60) {
+            System.out.print("Informe a nota da NFA: ");
+            final short NFA = input.nextShort();
+
+            if((NFA+NPA)/2 < 50) {
+                System.out.println("O aluno foi reprovado.");
+            }
+            else {
+                System.out.println("O aluno foi aprovado com sucesso!");
+            }
+        }
+        else {
+            System.out.println("O aluno foi aprovado com sucesso!");
+        }
+    }
+}
