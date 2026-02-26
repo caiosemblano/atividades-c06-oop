59df0e084db0f1d092ffacad46ff4cc081f03bfb
Caio Semblano Silva <semblanocaio@gmail.com>
2026-02-26 18:42:50 -0300

    Exercicio 6 completo

--- src/main/java/exercicio5/Main.java
+++ src/main/java/exercicio5/Main.java
@@ -1,4 +1,22 @@
 package exercicio5;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int nAlunos = input.nextInt();
+
+        switch(nAlunos){
+            case 10, 20:
+                System.out.println("A sala I-6 poderá ser utilizada.");
+                break;
+            case 30:
+                System.out.println("A sala I-5 deverá ser utilizada.");
+                break;
+            default:
+                System.out.println("A turma não possui o número mínimo de alunos.");
+                break; // Não sei se era pra ser isso
+        }
+    }
 }
