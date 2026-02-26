be77942e490d70013e551d53f04f645ca1346a45
Caio Semblano Silva <semblanocaio@gmail.com>
2026-02-25 22:01:40 -0300

    exercicio 2 completo

--- src/main/java/exercicio2/Main.java
+++ src/main/java/exercicio2/Main.java
@@ -3,12 +3,17 @@ package exercicio2;
 public class Main {
     public static void main(String[] args) {
         final short valorInteira = 30;
-        final short valorMeia = valorInteira/2;
+        final short valorMeia = valorInteira / 2;
 
         short inteirasVendidas = 8;
         short meiasVendidas = 5;
 
-        System.out.println("Total arrecadado pelas Inteiras vendidas: " + inteirasVendidas*valorInteira);
-        System.out.println("Total arrecadado pelas Meias vendidas: " + meiasVendidas*valorMeia);
+        System.out.println("Total arrecadado pelas Inteiras vendidas: " + inteirasVendidas * valorInteira);
+        System.out.println("Total arrecadado pelas Meias vendidas: " + meiasVendidas * valorMeia);
+        final int receitaTotal = (inteirasVendidas * valorInteira + meiasVendidas * valorMeia);
+        System.out.println("Total arrecadado foi: " + receitaTotal);
+        System.out.println("Media por ingresso vendido: " + (receitaTotal/(inteirasVendidas+meiasVendidas)));
 
+
+    }
 }
