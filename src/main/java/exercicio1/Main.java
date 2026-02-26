4f44e8d615ce40f274cd86e33acaec8be7701e89
Caio Semblano Silva <semblanocaio@gmail.com>
2026-02-25 21:54:57 -0300

    exercicio 1 completo

--- src/main/java/exercicio1/Main.java
+++ src/main/java/exercicio1/Main.java
@@ -2,6 +2,13 @@ package exercicio1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        short primeiraHora = 10;
+        short segundaHora = 4;
+        short terceiraHora = 2;
+
+        short total = 10 + 4 + 2;
+
+        System.out.println("O total de lanches consumidos foi: " + total);
+        System.out.println("A media de lanches consumidos por hora foi: " + total/3.0);
     }
 }
---
+++ src/main/java/exercicio2/Main.java
@@ -0,0 +1,14 @@
+package exercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+        final short valorInteira = 30;
+        final short valorMeia = valorInteira/2;
+
+        short inteirasVendidas = 8;
+        short meiasVendidas = 5;
+
+        System.out.println("Total arrecadado pelas Inteiras vendidas: " + inteirasVendidas*valorInteira);
+        System.out.println("Total arrecadado pelas Meias vendidas: " + meiasVendidas*valorMeia);
+
+}
