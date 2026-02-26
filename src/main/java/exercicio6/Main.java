29706b8929e815a52bc000464075f6c8954531b6
Caio Semblano Silva <semblanocaio@gmail.com>
2026-02-26 20:09:54 -0300

    Exercicio 6 completo

--- src/main/java/exercicio6/Main.java
+++ src/main/java/exercicio6/Main.java
@@ -1,4 +1,21 @@
 package exercicio6;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner input = new Scanner(System.in);
+
+        int chute;
+        int randomNum;
+        do {
+            System.out.print("Digite um número de 1 a 10: ");
+            randomNum = rand.nextInt(10) + 1;
+            chute = input.nextInt();
+        } while (chute != randomNum);
+
+        System.out.println("Parabens! Você acertou!!");
+    }
 }
