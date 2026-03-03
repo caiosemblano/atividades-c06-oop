60fa449d3503e74e0a2a6b810a258a3401446d98
Caio Semblano Silva <semblanocaio@gmail.com>
2026-03-03 00:04:48 -0300

    Questão 10 completa

--- src/main/java/AtividadesComNota/Exercicio1/Questao10.java
+++ src/main/java/AtividadesComNota/Exercicio1/Questao10.java
@@ -1,9 +1,31 @@
 package AtividadesComNota.Exercicio1;
 
+import java.util.Scanner;
+
 public class Questao10 {
     public static void main(String[] args) {
-        byte x = 127;
-        x++;
-        System.out.println(x);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o valor da Inteira: ");
+        String inputInteira = input.next();
+        System.out.print("Digite o valor da Meia-entrada: ");
+        String inputMeia = input.next() ;
+
+        inputInteira = inputInteira.replace(",",".");
+        inputMeia = inputMeia.replace(",",".");
+
+        double valorInteira = Double.parseDouble(inputInteira);
+        double valorMeia = Double.parseDouble(inputMeia);
+
+        System.out.print("Digite quantas Inteiras foram vendidas: ");
+        int inteirasVendidas = input.nextInt();
+        System.out.print("Digite quantas meias-entradas foram vendidas: ");
+        int meiasVendidas = input.nextInt();
+
+        System.out.println("Total arrecadado pelas Inteiras vendidas: " + inteirasVendidas * valorInteira);
+        System.out.println("Total arrecadado pelas Meias vendidas: " + meiasVendidas * valorMeia);
+        double receitaTotal = (inteirasVendidas * valorInteira + meiasVendidas * valorMeia);
+        System.out.println("Total arrecadado foi: " + receitaTotal);
+        System.out.println("Media por ingresso vendido: " + (receitaTotal/(inteirasVendidas+meiasVendidas)));
     }
 }
