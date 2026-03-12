1f4e8c56301da126db641c50450b6fb3e0fd210f
Caio Semblano Silva <semblanocaio@gmail.com>
2026-03-11 21:49:34 -0300

    Exercicio 3 completo

--- src/main/java/ExerciciosDeSala/capitulo3/exercicio3/Main.java
+++ src/main/java/ExerciciosDeSala/capitulo3/exercicio3/Main.java
@@ -1,4 +1,19 @@
 package ExerciciosDeSala.capitulo3.exercicio3;
 
 public class Main {
+    public static void main(String[] args) {
+        Zombie zombie1 = new Zombie(100, "Fulano");
+        Zombie zombie2 = new Zombie(100, "Ciclano");
+
+        zombie1.setHealth(20);
+        zombie2.setHealth(80);
+        System.out.println(zombie1.getHealth());
+        System.out.println(zombie2.getHealth());
+
+        zombie2.health = zombie1.health; // referenciando à vida do zumbi 1
+        zombie1.setHealth(200);
+        System.out.println(zombie1.getHealth());
+        System.out.println(zombie2.getHealth());
+
+    }
 }
