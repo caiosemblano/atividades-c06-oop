ffe05630e6ce35610535019631ee25d2baecd625
caiosemblano <semblanocaio@gmail.com>
2026-04-12 19:56:16 -0300

    Lista de Exercicios 2, valendo nota, completa

--- src/main/java/AtividadesComNota/Exercicio2/br/inatel/poo/Main.java
+++ src/main/java/AtividadesComNota/Exercicio2/br/inatel/poo/Main.java
@@ -7,12 +7,15 @@ public class Main {
     public static void main(String[] args) {
         Aluno aluno = new Aluno("Caio", "621", 98, 100, 95, 100);
         Aluno aluno1 = new Aluno("Fulano", "67", 99, 98, 65, 70);
+        Aluno aluno2 = new Aluno("Ciclano", "672", 50, 28, 85, 60);
         Turma pooComJava = new Turma();
 
         pooComJava.AdicionarAluno(aluno);
         pooComJava.AdicionarAluno(aluno1);
+        pooComJava.AdicionarAluno(aluno2);
         System.out.println(pooComJava.getQuantidadeAlunos() + "\n");
 
+        System.out.println("Listando todos os alunos:");
         pooComJava.listarAlunos();
         System.out.println();
         System.out.println();
